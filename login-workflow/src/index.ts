c06db386bb854b26f2ab6ae7078fe5e93a6268f5
joebochill <boyle.p.joseph@gmail.com>
2020-08-06 12:54:39 -0400

    Port RN specific elements from shared auth

--- login-workflow/src/index.ts
+++ login-workflow/src/index.ts
@@ -12,4 +12,7 @@ export type { AccountDetailInformation } from './subScreens/AccountDetails';
 export { ChangePassword } from './screens/ChangePassword';
 export { LoginHeaderSplash } from './components/LoginHeaderSplash';
 
+export { DismissKeyboardView } from './components/DismissKeyboardView';
+export {default as i18n } from './translations/i18n';
+
 export * from '@pxblue/react-auth-shared';
