4a6b5b225e8ca7db02109995d9ee558c62e94108
joebochill <boyle.p.joseph@gmail.com>
2020-11-10 14:47:20 -0500

    Use beta packages to verify updates

--- login-workflow/scripts/linkShared.sh
+++ login-workflow/scripts/linkShared.sh
@@ -15,18 +15,32 @@ cd ./shared-auth && yarn build && cd ..
 echo -en "${BLUE}Creating new folder in node_modules...${NC}"
 rm -rf "./node_modules/@pxblue/react-auth-shared"
 mkdir -p "./node_modules/@pxblue/react-auth-shared"
+
+rm -rf "./example/node_modules/@pxblue/react-auth-shared"
+mkdir -p "./example/node_modules/@pxblue/react-auth-shared"
 echo -e "${GREEN}Done${NC}"
 
-echo -en "${BLUE}Copying build output into workflow node_modules...${NC}";
+echo -en "${BLUE}Copying build output into node_modules...${NC}";
 cp -r ./shared-auth/package.json ./node_modules/@pxblue/react-auth-shared/package.json
 cp -r ./shared-auth/lib/. ./node_modules/@pxblue/react-auth-shared/lib
+
+cp -r ./shared-auth/package.json ./example/node_modules/@pxblue/react-auth-shared/package.json
+cp -r ./shared-auth/lib/. ./example/node_modules/@pxblue/react-auth-shared/lib
 echo -e "${GREEN}Done${NC}"
 
-echo -en "\r\n${BLUE}Linking Shared Auth to workflow: ${NC}"
+echo -en "\r\n${BLUE}Linking Shared Auth to workflow and example: ${NC}"
 if [ ! -f ./node_modules/@pxblue/react-auth-shared/package.json ]; then echo -e "${BRED}Not Linked${NC}" && exit 1; fi
 if [ ! -s ./node_modules/@pxblue/react-auth-shared ];
     then
         if [ ! -f ./node_modules/@pxblue/react-auth-shared/lib/commonjs/index.js ];
+        then echo -e "${BRED}Not Linked${NC}" && exit 1;
+        fi;
+fi
+
+if [ ! -f ./example/node_modules/@pxblue/react-auth-shared/package.json ]; then echo -e "${BRED}Not Linked${NC}" && exit 1; fi
+if [ ! -s ./example/node_modules/@pxblue/react-auth-shared ];
+    then
+        if [ ! -f ./example/node_modules/@pxblue/react-auth-shared/lib/commonjs/index.js ];
         then echo -e "${BRED}Not Linked${NC}" && exit 1;
         fi;
 fi
