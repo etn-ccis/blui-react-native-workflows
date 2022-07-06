ecc84a8bb42570953062cb547ef2f140a6239239
daileytj <daileytj89@gmail.com>
2022-07-06 15:12:24 -0400

    Update jestSetupFile

--- login-workflow/jestSetupFile.js
+++ login-workflow/jestSetupFile.js
@@ -1,5 +1,5 @@
 import mockAsyncStorage from '@react-native-async-storage/async-storage/jest/async-storage-mock';
-jest.mock('react-native/Libraries/Animated/src/NativeAnimatedHelper');
+jest.mock('react-native/Libraries/Animated/NativeAnimatedHelper');
 jest.mock('@react-native-async-storage/async-storage', () => mockAsyncStorage);
 jest.mock('react-i18next', () => ({
     useTranslation: () => ({ t: (key) => key }),
