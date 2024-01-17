cb5849e96c9d4993e007662bfbbe46ef2c50d9d4
ektaghag-eaton <EktaNGhag@eaton.com>
2024-01-17 14:13:32 +0530

    add test for workflowCardBody

---
+++ login-workflow/src/__tests__/components/WorkflowCard/WorkflowCardBody-test.tsx
@@ -0,0 +1,26 @@
+/**
+ * @format
+ */
+
+import React from 'react';
+
+import { cleanup } from '@testing-library/react-native';
+import renderer from 'react-test-renderer';
+import { WorkflowCardBody } from 'src/components/WorkflowCard';
+import { Text } from 'react-native-paper';
+
+describe('WorkflowCardBody Test', () => {
+    afterEach(cleanup);
+    it('WorkflowCardBody renders correctly', () => {
+        const rendered = renderer
+            .create(
+                <WorkflowCardBody>
+                    <Text>
+                        This is workflow Card Body content
+                    </Text>
+                </WorkflowCardBody>
+            )
+            .toJSON();
+        expect(rendered).toBeTruthy();
+    })
+});
