2129a40cde251f437a82f7fe14243184afa7ed74
Suraj Karambe <surajakarambe@eaton.com>
2024-01-19 14:19:54 +0530

    Added workflow card instructions component

---
+++ login-workflow/src/new_architecture/components/WorkflowCard/WorkflowCardInstructions.tsx
@@ -0,0 +1,52 @@
+import React from 'react';
+
+import { StyleSheet, ViewStyle, View } from 'react-native';
+import { Divider, Text, TextProps } from 'react-native-paper';
+
+export type WorkflowCardInstructionProps = Omit<TextProps<'bodyLarge'>, 'children' | 'theme' | 'variant'> & {
+    instructions?: string | JSX.Element;
+    divider?: boolean;
+};
+
+const makeStyles = (): StyleSheet.NamedStyles<{
+    container: ViewStyle;
+    workflowCardInstructions: ViewStyle;
+}> =>
+    StyleSheet.create({
+        container: {
+            marginHorizontal: 16,
+            paddingTop: 32,
+            paddingBottom: 40,
+        },
+        workflowCardInstructions: {
+            letterSpacing: 0,
+        },
+    });
+
+/**
+ * Component that renders the body content for the workflow card.
+ *
+ * @param children content to render in the WorkflowCardBody
+ *
+ * @category Component
+ */
+
+export const WorkflowCardInstructions: React.FC<WorkflowCardInstructionProps> = (props) => {
+    const { instructions, divider = true, style, ...otherProps } = props;
+    const styles = makeStyles();
+
+    return (
+        <>
+            <View style={[styles.container]}>
+                {typeof instructions === 'string' ? (
+                    <Text variant="bodyLarge" style={[styles.workflowCardInstructions, style]} {...otherProps}>
+                        {instructions}
+                    </Text>
+                ) : (
+                    instructions
+                )}
+            </View>
+            {divider && <Divider />}
+        </>
+    );
+};
