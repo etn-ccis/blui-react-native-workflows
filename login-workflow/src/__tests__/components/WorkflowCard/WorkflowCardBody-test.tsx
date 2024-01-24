d48d426ef4eca5858c1cd3e09419a80c9ac4fffc
ektaghag-eaton <EktaNGhag@eaton.com>
2024-01-24 15:21:21 +0530

    update test case

--- login-workflow/src/__tests__/components/WorkflowCard/WorkflowCardBody-test.tsx
+++ login-workflow/src/__tests__/components/WorkflowCard/WorkflowCardBody-test.tsx
@@ -3,22 +3,20 @@
  */
 
 import React from 'react';
-
-import { cleanup } from '@testing-library/react-native';
-import renderer from 'react-test-renderer';
+import { cleanup, render, screen } from '@testing-library/react-native';
 import { WorkflowCardBody } from 'src/components/WorkflowCard';
 import { Text } from 'react-native-paper';
 
 describe('WorkflowCardBody Test', () => {
     afterEach(cleanup);
     it('WorkflowCardBody renders correctly', () => {
-        const rendered = renderer
-            .create(
+        render(
                 <WorkflowCardBody>
                     <Text>This is workflow card body content.</Text>
                 </WorkflowCardBody>
-            )
+        )
             .toJSON();
-        expect(rendered).toBeTruthy();
+        expect(render).toBeTruthy();
+        expect(screen.getByText('This is workflow card body content.')).toBeTruthy();
     });
 });
