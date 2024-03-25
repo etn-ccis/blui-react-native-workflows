04c699e9bbce6e3b3ab604f6931367b8d62ebfb2
priyankakmEaton <priyankakm@eaton.com>
2024-03-25 12:32:18 +0530

    Removing WorkFlowCardFinishState component

--- login-workflow/src/screens/SuccessScreen/SuccessScreenBase.tsx
+++ login-workflow/src/screens/SuccessScreen/SuccessScreenBase.tsx
@@ -7,7 +7,20 @@ import {
     WorkflowCardInstructions,
 } from '../../components/WorkflowCard';
 import { SuccessScreenProps } from './types';
-import { WorkflowCardFinishState } from '../../components';
+import { EmptyState } from '@brightlayer-ui/react-native-components';
+import { View, StyleSheet, ViewStyle } from 'react-native';
+
+const makeStyles = (): StyleSheet.NamedStyles<{
+    emptyStateContainer: ViewStyle;
+}> =>
+    StyleSheet.create({
+        emptyStateContainer: {
+            display: 'flex',
+            justifyContent: 'center',
+            alignItems: 'center',
+            height: '100%',
+        },
+    });
 
 /**
  * Component that renders a success screen
@@ -18,20 +31,23 @@ import { WorkflowCardFinishState } from '../../components';
  */
 
 export const SuccessScreenBase: React.FC<SuccessScreenProps> = (props) => {
-    const { icon, messageTitle = '', message = '', dismissButtonLabel = '', canDismiss, onDismiss } = props;
+    const { icon, messageTitle = '', message = '', dismissButtonLabel = '', canDismiss, onDismiss , ...emptyStateProps} = props;
 
     const cardBaseProps = props.WorkflowCardBaseProps || {};
     const headerProps = props.WorkflowCardHeaderProps || {};
     const instructionsProps = props.WorkflowCardInstructionProps || {};
     const actionsProps = props.WorkflowCardActionsProps || {};
     const bodyProps = props.WorkflowCardBodyProps || {};
+    const styles = makeStyles();
 
     return (
         <WorkflowCard {...cardBaseProps}>
             {Object.keys(headerProps).length !== 0 && <WorkflowCardHeader {...headerProps} />}
             {Object.keys(instructionsProps).length !== 0 && <WorkflowCardInstructions {...instructionsProps} />}
             <WorkflowCardBody scrollable={false} {...bodyProps}>
-                <WorkflowCardFinishState icon={icon} title={messageTitle} description={message} />
+                <View style={[styles.emptyStateContainer]}>
+                    <EmptyState icon={icon} title={messageTitle} description={message} {...emptyStateProps} />
+                </View>
             </WorkflowCardBody>
             <WorkflowCardActions
                 {...actionsProps}
