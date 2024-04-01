8fb41b4687ca9ee7ebc288ce053b4939a9afcf69
ektaghag-eaton <EktaNGhag@eaton.com>
2024-04-01 14:14:22 +0530

    add changePassword component

--- login-workflow/example/src/screens/ChangePassword.tsx
+++ login-workflow/example/src/screens/ChangePassword.tsx
@@ -1,16 +1,41 @@
 import React from 'react';
-import { ChangePasswordScreen } from '@brightlayer-ui/react-native-auth-workflow';
+import { ChangePasswordScreen, AuthContextProvider } from '@brightlayer-ui/react-native-auth-workflow';
 import { useApp } from '../contexts/AppContextProvider';
 import { LocalStorage } from '../store/local-storage';
 import { useNavigation } from '@react-navigation/native';
+import { ProjectAuthUIActions } from '../actions/AuthUIActions';
+import i18nAppInstance from '../../translations/i18n';
 
 export const ChangePassword = (): JSX.Element => {
     const app = useApp();
-    // const { navigate } = useNavigation();
+    const nav = useNavigation();
     const logOut = (): void => {
         LocalStorage.clearAuthCredentials();
         app.onUserNotAuthenticated();
     };
 
-    return <ChangePasswordScreen onFinish={(): void => logOut()} showSuccessScreen />;
+    return (
+        <AuthContextProvider
+            language={app.language}
+            actions={ProjectAuthUIActions(app)}
+            i18n={i18nAppInstance}
+            navigate={(destination: -1 | string) => {
+                if (typeof destination === 'string') {
+                    nav.navigate(destination);
+                } else {
+                    nav.goBack();
+                }
+            }}
+            routeConfig={{
+                LOGIN: 'Login',
+                FORGOT_PASSWORD: undefined,
+                RESET_PASSWORD: undefined,
+                REGISTER_INVITE: undefined,
+                REGISTER_SELF: undefined,
+                SUPPORT: undefined,
+            }}
+        >
+            <ChangePasswordScreen onFinish={(): void => logOut()} showSuccessScreen />
+        </AuthContextProvider>
+    );
 };
