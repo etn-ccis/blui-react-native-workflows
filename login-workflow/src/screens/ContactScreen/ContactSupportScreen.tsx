a75b12a95ca451a7a8a8cc33c680e122851e14c1
ektaghag-eaton <EktaNGhag@eaton.com>
2024-04-02 18:56:30 +0530

    implement contact full screen

---
+++ login-workflow/src/screens/ContactScreen/ContactSupportScreen.tsx
@@ -0,0 +1,103 @@
+import React from 'react';
+import { ContactSupportScreenBase } from './ContactSupportScreenBase';
+import { ContactSupportScreenProps } from './types';
+import { useAuthContext } from '../../contexts';
+import { useTranslation } from 'react-i18next';
+import { Text } from 'react-native-paper';
+import { useExtendedTheme } from '@brightlayer-ui/react-native-themes';
+import { Linking } from 'react-native';
+
+/**
+ * Component renders a screen with contact information for support with the application.
+ * Contact information is pulled from the context passed into the workflow.
+ *
+ * @param {ContactSupportScreenProps} props - Props of Create Account Screen
+ *
+ * @category Component
+ */
+
+export const ContactSupportScreen: React.FC<ContactSupportScreenProps> = (props) => {
+    const { t } = useTranslation();
+    const { navigate } = useAuthContext();
+    const theme = useExtendedTheme();
+    const { contactEmail = 'something@email.com', contactPhone = '1-800-123-4567' } = props;
+
+    const defaultEmailSupportContent = (): JSX.Element => (
+        <Text variant="bodyLarge">
+            {`Contact Message `}
+            <Text
+                variant="labelLarge"
+                style={{ color: theme.colors.primary }}
+                onPress={(): any => Linking.openURL(`mailto:${contactEmail ?? ''}`)}
+            >
+                {contactEmail}
+            </Text>
+            {`.`}
+        </Text>
+    );
+
+    const defaultPhoneSupportContent = (): JSX.Element => (
+        <Text variant="bodyLarge">
+            {`Phone Message `}
+            <Text
+                variant="labelLarge"
+                style={{ color: theme.colors.primary }}
+                onPress={(): any => Linking.openURL(`tel:${contactPhone ?? ''}`)}
+            >
+                {contactPhone}
+            </Text>
+            {`.`}
+        </Text>
+    );
+
+    const {
+        icon = { name: 'chat-bubble-outline' },
+        iconSize = 70,
+        emailSupportTitle = t('bluiAuth:CONTACT_SUPPORT.GENERAL_QUESTIONS'),
+        emailSupportContent = defaultEmailSupportContent,
+        phoneSupportTitle = t('bluiAuth:CONTACT_SUPPORT.EMERGENCY_SUPPORT'),
+        phoneSupportContent = defaultPhoneSupportContent,
+        dismissButtonLabel = t('bluiCommon:ACTIONS.OKAY'),
+        onDismiss,
+        WorkflowCardHeaderProps,
+        WorkflowCardActionsProps,
+        ...otherContactSupportProps
+    } = props;
+
+    const workflowCardHeaderProps = {
+        title: t('bluiAuth:USER_MENU.CONTACT_US'),
+        onIconPress: (): void => {
+            navigate(-1);
+        },
+        ...WorkflowCardHeaderProps,
+    };
+
+    const workflowCardActionsProps = {
+        nextLabel: t('bluiCommon:ACTIONS.OKAY'),
+        showNext: true,
+        canGoNext: true,
+        fullWidthButton: true,
+        ...WorkflowCardActionsProps,
+        onNext: (): void => {
+            navigate(-1);
+            WorkflowCardActionsProps?.onNext?.();
+        },
+    };
+
+    return (
+        <ContactSupportScreenBase
+            WorkflowCardHeaderProps={workflowCardHeaderProps}
+            WorkflowCardActionsProps={workflowCardActionsProps}
+            icon={icon}
+            emailSupportTitle={emailSupportTitle}
+            emailSupportContent={emailSupportContent}
+            phoneSupportTitle={phoneSupportTitle}
+            phoneSupportContent={phoneSupportContent}
+            contactEmail={contactEmail}
+            contactPhone={contactPhone}
+            dismissButtonLabel={dismissButtonLabel}
+            onDismiss={onDismiss}
+            {...otherContactSupportProps}
+        />
+    );
+};
