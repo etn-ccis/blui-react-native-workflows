2f4ebe5f65493901a7c66678db5dc83d2895352f
Arshdeep Singh <arshdeepsingh@HMECMB00375.local>
2024-04-15 16:18:35 +0530

    fix testcases

--- login-workflow/jestSetupFile.js
+++ login-workflow/jestSetupFile.js
@@ -15,3 +15,14 @@ jest.mock('react-native-webview', () => {
         WebView: View,
     };
 });
+jest.mock('react-native', () => {
+    const RN = jest.requireActual('react-native');
+
+    RN.NativeModules.SettingsManager = {
+        settings: {
+            AppleLocale: 'en-US',
+            AppleLanguages: ['fr-FR', 'en-US'],
+        },
+    };
+    return RN;
+});
