ddf17b35007773559f3d113c4a16bde28ab9cf59
manojleaton <manojlokesh@eaton.com>
2024-04-16 22:36:14 +0530

    added bold to divider

--- login-workflow/src/components/WorkflowCard/WorkflowCardBody.tsx
+++ login-workflow/src/components/WorkflowCard/WorkflowCardBody.tsx
@@ -1,5 +1,5 @@
-import React, { useEffect } from 'react';
-import { ScrollView, StyleSheet, View, ViewStyle } from 'react-native';
+import React from 'react';
+import { StyleSheet, View, ViewStyle } from 'react-native';
 import { useScreenDimensions } from '../../hooks/useScreenDimensions';
 import { WorkflowCardBodyProps } from './WorkflowCard.types';
 import { Card } from 'react-native-paper';
