d93719a4b7f82eb3dd776b37471487c9fe2ddd39
manojleaton <manojlokesh@eaton.com>
2024-04-24 16:06:13 +0530

    removed navigation packages

--- login-workflow/jestSetupFile.js
+++ login-workflow/jestSetupFile.js
@@ -1,6 +1,4 @@
-import mockAsyncStorage from '@react-native-async-storage/async-storage/jest/async-storage-mock';
 import mockSafeAreaContext from 'react-native-safe-area-context/jest/mock';
-jest.mock('@react-native-async-storage/async-storage', () => mockAsyncStorage);
 jest.mock('react-native/Libraries/Animated/NativeAnimatedHelper');
 jest.mock('react-i18next', () => ({
     useTranslation: () => ({ t: (key) => key }),
@@ -8,7 +6,6 @@ jest.mock('react-i18next', () => ({
 }));
 jest.mock('react-native-reanimated', () => require('react-native-reanimated/mock'));
 jest.mock('react-native-safe-area-context', () => mockSafeAreaContext);
-jest.mock('@react-navigation/native');
 jest.mock('react-native-webview', () => {
     const { View } = require('react-native');
     return {
