dd7248bd0729d61f7d771d0538579141fa87a9ae
manojleaton <manojlokesh@eaton.com>
2024-04-25 15:54:23 +0530

    packages removed in example project

--- login-workflow/example/metro.config.js
+++ login-workflow/example/metro.config.js
@@ -4,17 +4,12 @@ const defaultConfig = getDefaultConfig(__dirname);
 
 const config = {
     transformer: {
-        babelTransformerPath: require.resolve('react-native-svg-transformer'),
         getTransformOptions: async () => ({
             transform: {
                 experimentalImportSupport: false,
                 inlineRequires: true,
             },
         }),
-    },
-    resolver: {
-        assetExts: defaultConfig.resolver.assetExts.filter((ext) => ext !== 'svg'),
-        sourceExts: [...defaultConfig.resolver.sourceExts, 'svg'],
     },
 };
 
