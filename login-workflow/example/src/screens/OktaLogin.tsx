2b19a40868cbe1547865df75468867a0c01da99c
shubham-eaton <ShubhamkVishwakarma@eaton.com>
2024-08-29 13:57:59 +0530

    Lint and unit test issue fixes

--- login-workflow/example/src/screens/OktaLogin.tsx
+++ login-workflow/example/src/screens/OktaLogin.tsx
@@ -2,16 +2,14 @@ import React from 'react';
 import { Image } from 'react-native';
 import { OktaLoginScreenProps, OktaLoginScreen } from '@brightlayer-ui/react-native-auth-workflow';
 
-export const OktaLogin: React.FC<React.PropsWithChildren<OktaLoginScreenProps>> = () => {
-    return (
-        <OktaLoginScreen
-            projectImage={
-                <Image
-                    style={{ width: '100%' }}
-                    resizeMode="contain"
-                    source={require('../assets/images/eaton_stacked_logo.png')}
-                />
-            }
-        />
-    );
-};
+export const OktaLogin: React.FC<React.PropsWithChildren<OktaLoginScreenProps>> = () => (
+    <OktaLoginScreen
+        projectImage={
+            <Image
+                style={{ width: '100%' }}
+                resizeMode="contain"
+                source={require('../assets/images/eaton_stacked_logo.png')}
+            />
+        }
+    />
+);
