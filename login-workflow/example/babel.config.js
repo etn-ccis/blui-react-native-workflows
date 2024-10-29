a607b44d199f05039db14094294fce0f7a8044a4
shubham-eaton <ShubhamkVishwakarma@eaton.com>
2024-10-29 11:57:08 +0530

    Fixed unit testing failure

--- login-workflow/example/babel.config.js
+++ login-workflow/example/babel.config.js
@@ -1,5 +1,5 @@
 module.exports = {
     presets: ['module:@react-native/babel-preset'],
     // react-native-reanimated/plugin must be listed last
-    plugins: ['react-native-reanimated/plugin', 'module:react-native-dotenv',],
+    plugins: ['react-native-reanimated/plugin', 'module:react-native-dotenv'],
 };
